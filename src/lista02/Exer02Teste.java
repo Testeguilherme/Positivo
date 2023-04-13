affd519e2dff82f1eec37e64584f1d5cf73409dd
unknown <creusodagama3@hotmail.com>
2023-04-13 16:33:51 -0300

    msg

--- src/lista02/Exer02Teste.java
+++ src/lista02/Exer02Teste.java
@@ -5,6 +5,7 @@ public class Exer02Teste {
 	public static void main(String[] args) {
 		
 		Exer02 e2 = new Exer02(1.0, 6.0, 5.0);
+		
 		System.out.println(e2.calculaMediaPonderada()); 
 		System.out.println(e2.verificaSituacaoAluno());
 		
--- src/lista02/Exer03Teste.java
+++ src/lista02/Exer03Teste.java
@@ -4,8 +4,13 @@ public class Exer03Teste {
 	public static void main(String[] args) {
 
 		Exer03 e3 = new Exer03(1500.0, 3.0);
-		
-		
-		
+		System.out.println("Estoque inicial: " + e3.getQtde());
+		System.out.println("Preco total: " + e3.valorTotalEstoque());
+		e3.diminuiEstoque(1.0);
+		System.out.println("Estoque final: " + e3.getQtde());
+		System.out.println("Preco total: " + e3.valorTotalEstoque());
+		e3.aumentaEstoque(5d);
+		System.out.println("Estoque final2: " + e3.getQtde());
+		System.out.println("Preco total: " + e3.valorTotalEstoque());
 	}
 }
